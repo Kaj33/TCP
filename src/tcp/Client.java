3fcd1a5ab87de465b63b0881acc238b89f33efa0
kaj33 <kaj33@localhost>
2024-03-19 11:08:48 +0100

    Aggiunta file README

---
+++ src/tcp/Client.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tcp;
+
+/**
+ *
+ * @author kaj33
+ */
+public class Client {
+
+    private String nome;
+    private String colore;
+
+    public Client(String nome, String colore) {
+        this.nome = nome;
+        this.colore = colore;
+    }
+
+    
+    public void connetti(String nomeServer, int portaServer) {
+        // Implementazione del metodo connetti()
+    }
+
+    
+    public void scrivi(String dati) {
+        // Implementazione del metodo scrivi()
+    }
+
+    
+    public String leggi() {
+        // Implementazione del metodo leggi()
+        return null;
+        // Implementazione del metodo leggi()
+    }
+
+    
+    public void chiudi() {
+        // Implementazione del metodo chiudi()
+    }
+
+}
