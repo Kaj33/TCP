003d2161db5348bf0a3df4c52f976e55b804d836
kaj33 <kaj33@localhost>
2024-03-26 10:23:29 +0100

    Avanzamento 3

--- src/tcp/Client.java
+++ src/tcp/Client.java
@@ -6,6 +6,7 @@ package tcp;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
